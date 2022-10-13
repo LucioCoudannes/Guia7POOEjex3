2da23236249762f610acdd570001a4edf101e9c7
Lucio Coudannes <luciocoudannes@gmail.com>
2022-10-13 17:00:28 -0300

    resuelto

---
+++ src/Servicios/RaicesServicio.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+import Entidades.Raices;
+
+/**
+ *
+ * @author A288808
+ */
+public class RaicesServicio {
+
+    double resultado;
+
+    public double Discriminante(Raices a, Raices b, Raices c) {
+
+        resultado = (Math.pow(b.getB(), 2)) - (4 * a.getA() * c.getC());
+
+        return resultado;
+
+    }
+
+    public Boolean tieneRaices() {
+
+        return resultado > 0;
+
+    }
+
+    public Boolean tieneRaiz() {
+
+        return resultado == 0;
+
+    }
+
+    public void obtenerRaices(Raices a, Raices b, Raices c) {
+
+        if (this.tieneRaices() == true) {
+
+            System.out.println("El valor de la discriminante es " + (Math.pow(b.getB(), 2) - 4 * a.getA() * c.getC()));
+
+            System.out.print("El prmier resultado es ");
+            System.out.println((-b.getB() + Math.sqrt(Math.pow(b.getB(), 2) - 4 * a.getA() * c.getC())) / (2 * a.getA()));
+            System.out.print("El segundo resultado es ");
+            System.out.println((-b.getB() - Math.sqrt(Math.pow(b.getB(), 2) - 4 * a.getA() * c.getC())) / (2 * a.getA()));
+
+        } else {
+
+            System.out.println("No tiene raiz");
+        }
+
+    }
+
+    public void obtenerRaiz(Raices a, Raices b, Raices c) {
+
+        if (this.tieneRaiz() == true) {
+
+            System.out.print("EL unico valor posible es ");
+            System.out.println((-b.getB() - Math.sqrt(0)) / (2 * a.getA()));
+
+        }
+
+    }
+    
+    
+
+}
---
+++ src/guia7pooejex3/Guia7POOEjex3.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7pooejex3;
+
+import Entidades.Raices;
+import Servicios.RaicesServicio;
+import java.util.Scanner;
+
+/**
+ *
+ * @author A288808
+ */
+public class Guia7POOEjex3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        RaicesServicio rs = new RaicesServicio();
+
+        Raices obj1 = new Raices();
+
+        System.out.println("Ingrese el valor de A");
+        obj1.setA(leer.nextInt());
+
+        System.out.println("Ingrese el valor de B");
+        obj1.setB(leer.nextInt());
+
+        System.out.println("Ingrese el valor de C");
+        obj1.setC(leer.nextInt());
+
+        rs.Discriminante(obj1, obj1, obj1);
+
+        System.out.println(obj1.toString());
+
+        rs.tieneRaices();
+
+        if (rs.tieneRaices() == true) {
+
+            System.out.println("VERDADERO");
+
+        } else {
+
+            System.out.println("FALSO");
+
+        }
+
+        rs.tieneRaiz();
+
+        if (rs.tieneRaiz() == true) {
+
+            System.out.println("VERDADERO");
+
+        } else {
+
+            System.out.println("FALSO");
+
+        }
+        
+        rs.obtenerRaices(obj1, obj1, obj1);
+        
+        rs.obtenerRaiz(obj1, obj1, obj1);
+        
+        
+                
+        
+
+    }
+
+}
